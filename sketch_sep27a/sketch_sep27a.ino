9df23b631d6812d433b5c7c947977b586de98da9
Nioddo <167439680+Nioddo@users.noreply.github.com>
2024-10-21 16:17:07 -0300

    mapa agregado

--- sketch_sep27a/sketch_sep27a.ino
+++ sketch_sep27a/sketch_sep27a.ino
@@ -1,27 +1,23 @@
 #include <Adafruit_NeoPixel.h>
- 
+
 #define PIN 11 // Pin donde está conectado el neopixel
 #define NUMPIXELS 5 // Número de neopixels
- 
+
 Adafruit_NeoPixel pixels(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
- 
+
 void setup() {
     Serial.begin(9600);
     pixels.begin();
 }
- 
+
 void loop() {
-    if (Serial.available()) {
-        String command = Serial.readStringUntil('\n');
-        int ledIndex = command.substring(0, command.indexOf(':')).toInt();
-        String rgbValues = command.substring(command.indexOf(':') + 1);
-        int r = rgbValues.substring(0, rgbValues.indexOf(',')).toInt();
-        int g = rgbValues.substring(rgbValues.indexOf(',') + 1, rgbValues.lastIndexOf(',')).toInt();
-        int b = rgbValues.substring(rgbValues.lastIndexOf(',') + 1).toInt();
-        
-        // Establecer el color del LED correspondiente
-        pixels.setPixelColor(ledIndex, pixels.Color(r, g, b));
+    if (Serial.available() >= 15) { // Espera 15 bytes (R, G, B x 5)
+        for (int i = 0; i < NUMPIXELS; i++) {
+            int red = Serial.read();
+            int green = Serial.read();
+            int blue = Serial.read();
+            pixels.setPixelColor(i, pixels.Color(red, green, blue));
+        }
         pixels.show();
     }
 }
- 
